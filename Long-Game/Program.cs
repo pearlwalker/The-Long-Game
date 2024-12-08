57bb002842b742132348d53343569524eb0ff40d
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-12-08 15:32:51 -0500

    Scripted game introduction
    Declared scope and name variables, got player's name, displayed player's score, explained game in console messages

--- Long-Game/Program.cs
+++ Long-Game/Program.cs
@@ -10,7 +10,21 @@ namespace Long_Game
     {
         static void Main(string[] args)
         {
+            //Variables
+            int score = 0;
+            string name = null;
 
+
+            Console.WriteLine("Hello Player, what's your name?");
+            name = Console.ReadLine();
+
+
+            Console.WriteLine("\nYour current score is: " + score);
+            Console.WriteLine("To increase your score, press any key");
+            Console.WriteLine("When you're ready to end the game, press Enter");
+            Console.WriteLine("\nWhen you're ready to start, press Enter");
+            Console.ReadLine();
+            Console.WriteLine("Alright " + name + ", let's play!");
         }
     }
 }
